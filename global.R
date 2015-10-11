e462ce19714475f4763c97f6d0ae118ec5772a8e
shrektan <shrektan@126.com>
2015-10-11 16:08:01 +0800

    rewrite the data save/modify/load structure also, split the info_maintain code from server.R also delete packrat data

--- global.R
+++ global.R
@@ -11,6 +11,7 @@ library(DT)
 library(shinyjs)
 library(shinyBS)
 library(leaflet)
+library(readr)
 
 # loading css -------------------------------------------------------------
 
@@ -32,11 +33,68 @@ color: #FFFFFF;
 # na2blank
 na2blank <- function(x) ifelse(is.na(x), "", x)
 
+# define error message
+shinyjs_validate <- function(...) {
+  ls <- list(...)
+  tmp <- vapply(ls, is.null, FUN.VALUE = TRUE)
+  ls <- ls[which(!tmp)]
+  if (length(ls) == 0) return(invisible())
+  shinyjs::info(paste0(ls, collapse = " "))
+  cond <- structure(
+    list(message = ""), 
+    class = c("validation", 
+              "shiny.silent.error", "error", "condition"))
+  stop(cond)
+}
+
 # data --------------------------------------------------------------------
 
-# read data
-dt <- fread("data.csv", encoding = "UTF-8")
-dt_col <- fread("colname_cn.csv", encoding = "UTF-8") %>% setkey(EN)
+dt <- dt_col <- NULL
 
+# load data
+load_data <- function() {
+  tmp <- 
+    read_csv("data/data.csv", 
+             col_types = cols(ifDeleted = col_logical(), 
+                              TimeStamp = col_datetime())) %>%
+    setDT()
+  dt <<- 
+    tmp[, MaxTimeStamp := max(TimeStamp), by = Name][
+      MaxTimeStamp == TimeStamp
+    ][, MaxTimeStamp := NULL][ifDeleted == FALSE]
+  dt_col <<- fread("data/colname_cn.csv", encoding = "UTF-8") %>% setkey(EN)
+  invisible()
+}
+
+load_data()
+
+# define backup data
+backup_data <- function() {
+  if (!dir.exists("./csv_his")) dir.create("./csv_his")
+  backup_file <- 
+    file.path(
+      "./csv_his", 
+      paste0(format(Sys.time(), "%Y%m%d%H%M%S"),
+             basename(tempfile(pattern = "")), ".csv")
+    )
+  file.copy("./data/data.csv", backup_file)
+  invisible()
+}
 # establish server push
 server_push <- 0L
+
+# update data
+update_data <- function(r) {
+  # backup
+  backup_data()
+  # write csv
+  write_csv(r[, colnames(dt), with = FALSE], 
+            "data/data.csv", 
+            append = file.exists("data/data.csv"))
+  # read csv
+  load_data()
+  # server push
+  server_push <<- server_push + 1L
+  # return
+  invisible()
+}
---
+++ info_maintain.R
@@ -0,0 +1,124 @@
+# info maintenance
+# define reset_info
+reset_info <- function() {
+  shinyjs::reset("info_class")
+  shinyjs::reset("info_class_en")
+  shinyjs::reset("info_name")
+  shinyjs::reset("info_name_en")
+  shinyjs::reset("info_area")
+  shinyjs::reset("info_country")
+  shinyjs::reset("info_lng")
+  shinyjs::reset("info_lat")
+  session$sendInputMessage("info_address", list(value = ""))
+  session$sendInputMessage("info_website", list(value = ""))
+}
+# define reload info
+reload_info <- function(info_target) {
+  dt_tgt <- data() %>% dplyr::filter(Name == input$info_target)
+  updateSelectizeInput(session, "info_class", selected = dt_tgt[, Class])
+  updateSelectizeInput(session, "info_class_en", selected = dt_tgt[, Class_EN])
+  updateTextInput(session, "info_name", value = dt_tgt[, Name])
+  updateTextInput(session, "info_name_en", value = dt_tgt[, Name_EN])
+  updateSelectizeInput(session, "info_area", selected = dt_tgt[, Area])
+  updateSelectizeInput(session, "info_country", selected = dt_tgt[, Country])
+  updateNumericInput(session, "info_lng", value = dt_tgt[, LNG])
+  updateNumericInput(session, "info_lat", value = dt_tgt[, LAT])
+  session$sendInputMessage("info_address", list(value = dt_tgt[, Address]))
+  session$sendInputMessage("info_website", list(value = dt_tgt[, Website]))
+}
+
+# define read_info
+read_info <- function() {
+  data.table(
+    Class = input$info_class,
+    Class_EN = input$info_class_en,
+    Name = input$info_name,
+    Name_EN = input$info_name_en,
+    Area = input$info_area,
+    Country = input$info_country,
+    Address = input$info_address,
+    Website = input$info_website,
+    LNG = input$info_lng,
+    LAT = input$info_lat,
+    ifDeleted = FALSE,
+    TimeStamp = Sys.time() %>% lubridate::with_tz(tzone = "UTC")
+  )
+}
+# if add then clear info; if modify then load info
+observe({
+  if (input$info_mode == "add") {
+    isolate({
+      reset_info()
+    })
+  }
+})
+observe(
+  if (input$info_mode == "modify") {
+    reload_info(input$info_target)
+  }
+)
+
+# define check_info
+check_info <- function() {
+  shinyjs_validate(
+    need(input$info_lng, "LNG needs a number!"),
+    need(input$info_lat, "LAT needs a number!"),
+    need(input$info_name, "Need a name."),
+    need(input$info_name_en, "Need an english name."),
+    need(input$info_class, "Need a class."),
+    need(input$info_class_en, "Need an english class.")
+  )
+}
+# submit info maintenance
+observeEvent(
+  input$info_submit,
+  switch(
+    input$info_mode,
+    "add" = {
+      check_info()
+      # check if duplicate info
+      new <- read_info()
+      shinyjs_validate(
+        need(!(new$Name %in% dt$Name), "Duplicate Chinese Name!"),
+        need(!(new$Name_EN %in% dt$Name_EN), "Duplicate English Name!")
+      )
+      # rbind
+      update_data(new)
+      # reset info
+      reset_info()
+    },
+    "modify" = {
+      check_info()
+      # check if duplicate info
+      new <- read_info()
+      shinyjs_validate(
+        need(!(new$Name %in% dt$Name[dt$Name != input$info_target]),
+             "Duplicate Chinese Name!"),
+        need(!(new$Name_EN %in% dt$Name_EN[dt$Name != input$info_target]), 
+             "Duplicate English Name!")
+      )
+      # delete old
+      r <- dt %>% 
+        dplyr::filter(Name == input$info_target) %>%
+        dplyr::mutate(
+          ifDeleted = TRUE,
+          TimeStamp = Sys.time() %>% lubridate::with_tz("UTC")
+        )
+      # add new
+      r <- rbindlist(list(r, new), use.names = TRUE)
+      update_data(r)
+      # reload info
+      reload_info(input$info_target)
+    },
+    "delete" = {
+      # delete old
+      r <- dt %>% 
+        dplyr::filter(Name == input$info_target) %>%
+        dplyr::mutate(
+          ifDeleted = TRUE,
+          TimeStamp = Sys.time() %>% lubridate::with_tz("UTC")
+        )
+      update_data(r)
+    }
+  )
+)
