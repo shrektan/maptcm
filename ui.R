a17e2e95e2397526d4157ca53e2f5704c88fc395
shrektan <shrektan@126.com>
2015-10-20 23:26:57 +0800

    improve github ribbon

--- ui.R
+++ ui.R
@@ -42,7 +42,11 @@ navbarPage(
         ),
         p(
           HTML(
-            'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>'
+            'Icons made by <a href="http://www.freepik.com" 
+            title="Freepik">Freepik</a> from 
+            <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> 
+            is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0">CC BY 3.0</a>'
           )
         )
       )
@@ -86,10 +90,11 @@ navbarPage(
       href = "https://github.com/shrektan/CMUnivMap",
       target = "_blank",
       tags$img(
-        style = "position: absolute; top: 0; right: 0; border: 0;z-index : 5000",
-        src = "https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67",
+        style = "position: absolute; top: 0; right: 0; border: 0; z-index: 5000",
+        src = "github-label-grey.png",
         alt = "Fork me on GitHub",
-        `data-canonical-src` = "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+        `data-canonical-src` = 
+          "https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
       )
     )
   )
